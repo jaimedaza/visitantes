e8cbfa54522eaee0639c645c6e822de30bd8d302
Jaime Daza <jaimedaza11@gmail.com>
2020-04-16 10:17:14 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/visitantes', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+const visitorSchema = new mongoose.Schema({
+
+  date: { type: Date, default: Date.now },
+  name: String
+  
+});
+
+const Visitor = mongoose.model('Visitor', visitorSchema);
+
+
+app.get('/', (req, res) => { 
+
+	let name = req.query.name;
+
+	if(name === undefined){
+		name = "Anónimo";
+	}
+
+	const person = new Visitor({		
+		name: name
+	});
+
+	person.save(function(error){
+		console.log(error);
+		return error;
+	});
+
+	res.send('<h1>El visitante fue almacenado con éxito</h1>');
+
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
+
+
