52f0cc4b0eaa9bc237993c52aadcfa2cabe28c5a
Jaime Daza <jaimedaza11@gmail.com>
2020-04-16 18:28:30 -0500

    add connection error

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/visitante
 	useNewUrlParser: true,	
 });
 
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+
 const visitorSchema = new mongoose.Schema({
 
   date: { type: Date, default: Date.now },
